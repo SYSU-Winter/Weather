b6ab79ce9a5de0eb963ac0afa133ccbd5b91e3fb
“““SYSU-Winter git config --global user.email “Yliuwt1314@gmail.com <liuwt1314@gmail.com>
2017-03-23 09:59:50 +0800

    Add some annotation

--- app/src/main/java/sysu/lwt/myweather/WeatherAdapter.java
+++ app/src/main/java/sysu/lwt/myweather/WeatherAdapter.java
@@ -33,6 +33,14 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
         mInflater = LayoutInflater.from(context);
     }
 
+    /**
+     * WeatherAdapter是继承自RecyclerView.Adapter的，必须重写
+     * onCreateViewHolder(), onBindViewHolder(), getItemCount()方法
+     */
+
+    // 用于创建ViewHolder实例，在这个方法中，将weather_item布局加载进来
+    // 然后创建一个ViewHolder实例，并把加载出来的布局传入到构造函数中，
+    // 最后将ViewHolder的实例返回
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         View view = mInflater.inflate(R.layout.weather_item, viewGroup, false);
@@ -43,6 +51,9 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
         return holder;
     }
 
+    // 用于对RecyclerView子项的数据进行赋值，
+    // 会在每个子项被滚动到屏幕内时执行
+    // 这里通过参数i得到当前项的weather实例，然后再将数据设置到ViewHolder的TextView中
     @Override
     public void onBindViewHolder(final ViewHolder viewHolder, final int i) {
         viewHolder.Date.setText(weather_list.get(i).getData());
@@ -59,12 +70,16 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.ViewHold
         }
     }
 
+    // 用于告诉RecyclerView一共有多少子项
     @Override
     public int getItemCount() {
         return weather_list.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        // ViewHolder的构造函数中传入一个View参数，这个参数就是RecyclerView子项
+        // 的最外层布局，这样
+        // 我们就可以通过findViewById()的方法来获取到布局中的各种控件的实例了
         public ViewHolder(View itemView) {
             super(itemView);
         }
